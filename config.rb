c72fed0543e7f5caf6fe3bae6728f0f3f0ab0ef0
Rob Carter <robbie.carter@gmail.com>
2017-04-12 17:05:40 +0200

    Changed vape url

--- config.rb
+++ config.rb
@@ -13,7 +13,7 @@ page '/*.txt', layout: false
 # page "/path/to/file.html", layout: :otherlayout
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-#proxy "/", "/tobacco-companies-furious.html"
+proxy "/", "/Firefly2.html"
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
